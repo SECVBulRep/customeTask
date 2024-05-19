274579788ad3a63557e4b528cd7d87db172648ac
SECVBulRep <bfarukshin@gmail.com>
2024-05-19 17:52:37 +0300

    * step 1
    Why always 100 ?

--- MyTask/MyTask/Program.cs
+++ MyTask/MyTask/Program.cs
@@ -1,5 +1,12 @@
 ﻿
 
-
+for (int i = 0;  i < 100;i++)
+{
+    ThreadPool.QueueUserWorkItem(delegate
+    {
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+    });
+}
 
 Console.ReadKey();
