8d1cbcaf29eec28fe31fbce2021e62a03af75fee
SECVBulRep <bfarukshin@gmail.com>
2024-05-19 18:17:05 +0300

    свой ThreadPool

--- MyTask/MyTask/Program.cs
+++ MyTask/MyTask/Program.cs
@@ -1,16 +1,46 @@
-﻿
+﻿using System.Collections.Concurrent;
 
-for (int i = 0;  i < 100;i++)
+for (int i = 0; i < 100; i++)
 {
     var captured = i;
-    ThreadPool.QueueUserWorkItem(delegate
+    MyThreadPool.QueueUserWorkItem(delegate
     {
         Console.WriteLine(captured);
         Thread.Sleep(1000);
     });
 }
 
+
 Console.WriteLine("-- end --." +
                   "press any key ...");
 
 Console.ReadKey();
+
+
+static class MyThreadPool
+{
+    private static readonly BlockingCollection<Action> s_workItems = new();
+
+    public static void QueueUserWorkItem(Action action)
+    {
+        s_workItems.Add(action);
+    }
+
+    static MyThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    var action = s_workItems.Take();
+                    action();
+                }
+            })
+            {
+                IsBackground = false
+            }.Start();
+        }
+    }
+}
