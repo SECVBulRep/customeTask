ae4842f1a1ec48fa21cca41ee29633281b6b76d5
SECVBulRep <bfarukshin@gmail.com>
2024-05-19 18:06:35 +0300

    перенесли ввнутренний скоуп

--- MyTask/MyTask/Program.cs
+++ MyTask/MyTask/Program.cs
@@ -2,11 +2,15 @@
 
 for (int i = 0;  i < 100;i++)
 {
+    var captured = i;
     ThreadPool.QueueUserWorkItem(delegate
     {
-        Console.WriteLine(i);
+        Console.WriteLine(captured);
         Thread.Sleep(1000);
     });
 }
+
+Console.WriteLine("-- end --." +
+                  "press any key ...");
 
 Console.ReadKey();
